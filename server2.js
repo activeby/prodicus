692f839455c6675705add0ee29715eb88ea048c6
Mikhail Kytyzov <mikhail.kytyzov@activeplatform.com>
2017-04-18 14:34:45 +0300

    add support

--- server2.js
+++ server2.js
@@ -26,23 +26,19 @@ var bot = new builder.UniversalBot(connector, function (session) {
 var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/b8ac99e4-3e42-4bb4-8fa3-eec10e4de189?subscription-key=88ec7ba1fb364eb590fdb5764bf73f7f&timezoneOffset=0.0&verbose=true&q=');
 bot.recognizer(recognizer);
 
-
 bot.dialog('help', function (session) {
-    session.send('Hi!');
+    if(session.message.user.id != 'default-user'){
+      session.send('Hi, %s', session.message.user.name)
+    } else {
+      session.send('Hi!');
+    }
+
     var action = createRootMenu(session);
 
     var msg = new builder.Message(session).addAttachment(action);
     session.endDialog(msg);
 }).triggerAction({
     matches: 'help'
-});
-
-bot.dialog('domain', [
-  function (session, args, next) {
-    session.send(' \'%s\'', session.message.text);
-  },
-]).triggerAction({
-    matches: 'domain'
 });
 
 bot.dialog('Buy', [
@@ -64,7 +60,125 @@ bot.dialog('Buy', [
     }
 });
 
+bot.dialog('Support', [
+  function (session, args, next) {
+    var domain = builder.EntityRecognizer.findEntity(args.intent.entities, 'domain');
+    var hosting = builder.EntityRecognizer.findEntity(args.intent.entities, 'hosting');
+    var cloud = builder.EntityRecognizer.findEntity(args.intent.entities, 'cloud');
+
+    if(cloud){
+      session.send('Oh, i see u have problem with clouds')
+      session.endDialog();
+      session.beginDialog('cloudchannel', cloud)
+    } else if (hosting) {
+      session.send('Oh, i see u have problem with hosting')
+      session.endDialog();
+      session.beginDialog('hostingchannel', hosting)
+    } else if (domain) {
+      session.send('Oh, i see u have problem with domains')
+      session.endDialog();
+      session.beginDialog('domainchannel', domain)
+    } else {
+      let action = buildSupportMenu(session);
+
+      let msg = new builder.Message(session).addAttachment(action);
+      session.endDialog(msg);
+    }
+
+  }
+]).triggerAction({
+  matches: 'Support',
+  onInterrupted: function (session) {
+    session.send('something went wrong');
+  }
+});
+
+bot.dialog('cloudchannel', [
+  function(session, args, next){
+    session.send('there is cloud support channel');
+    let action = buildEntMenu(session);
+    let msg = new builder.Message(session).addAttachment(action);
+    session.send(msg);
+
+    builder.Prompts.text(session, 'Describe your problem or follow buttons');
+  },
+  function(session, results){
+    let link = 'http://www.active.by/ru-by/it-consulting/sla.html'
+    session.endDialog('Now i am not so smart, follow the link %s', link)
+  }
+]);
+
+bot.dialog('hostingchannel', [
+  function(session, args, next){
+    session.send('there is hosting support channel');
+    let action = buildEntMenu(session);
+    let msg = new builder.Message(session).addAttachment(action);
+    session.send(msg);
+    builder.Prompts.text(session, 'Describe your problem');
+  },
+  function(session, results){
+    let link = 'http://www.active.by/ru-by/services/bitrixhost/'
+    session.endDialog('Now i am not so smart, follow the link %s', link)
+  }
+]);
+bot.dialog('domainchannel', [
+  function(session, args, next){
+    session.send('there is domain support channel');
+    let action = buildEntMenu(session);
+    let msg = new builder.Message(session).addAttachment(action);
+    session.send(msg);
+    builder.Prompts.text(session, 'Describe your problem');
+  },
+  function(session, results){
+    let link = 'http://www.active.by/ru-by/services/domains/'
+    session.endDialog('Now i am not so smart, follow the link %s', link)
+  }
+]);
+
+bot.dialog('mailbox', [
+  function(session, args, next){
+    session.endDialog('To change ur mailbox size or other manipulations follow the link, %s', 'https://link_to_mailbox.com');
+  }
+]).triggerAction({
+  matches: 'mailbox',
+  onInterrupted: function (session) {
+    session.send('something went wrong');
+  }
+});
+
+bot.dialog('profile', [
+  function(session, args, next){
+    session.endDialog('To make manipulations with profile or user, follow link %s', 'https://profile_edit.com');
+  }
+]).triggerAction({
+  matches: 'profile',
+  onInterrupted: function (session) {
+    session.send('something went wrong');
+  }
+});
+
 // helpers
+function buildSupportMenu(session){
+  const card = new builder.ThumbnailCard(session);
+    card.buttons([
+        new builder.CardAction(session).title('support hosting').value('support hosting').type('imBack'),
+        new builder.CardAction(session).title('support domain').value('support domain').type('imBack'),
+        new builder.CardAction(session).title('support cloud').value('support cloud').type('imBack'),
+    ]).text(`Which type of help do you need?`);
+
+  return card;
+}
+
+function buildEntMenu(session){
+  const card = new builder.ThumbnailCard(session);
+    card.buttons([
+        new builder.CardAction(session).title('doesnt work').value('doesnt work').type('imBack'),
+        new builder.CardAction(session).title('cant buy').value('cant buy').type('imBack'),
+        new builder.CardAction(session).title('additional').value('additional').type('imBack')
+    ]).text(`Which type of help do you need?`);
+
+  return card;
+}
 
 function createRootMenu(session) {
   const card = new builder.ThumbnailCard(session);
